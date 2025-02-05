3444e72ad3aed7d2f9324a296217080a6df35d53
user-pratik <pratikanand113@gmail.com>
2025-02-06 01:19:42 +0530

    Revert "Removed secret from index.html"
    This reverts commit e2f5e686aa5849c847c3a9f948d700a21ebea70c.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,30 @@
-const response = await fetch("https://api.github.com/repos/user-pratik/portfolio/issues", {
-    method: "POST",
-    headers: {
-        "Authorization": "Bearer " + process.env.GH_TOKEN,
-        "Accept": "application/vnd.github.v3+json",
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-        title: "New Feedback",
-        body: JSON.stringify(data, null, 2),
-        labels: ["feedback"]
-    })
+$(function() {
+    let viewport = $('#container').innerWidth();
+    let tb = $('#thumbs');
+    let divWidth = tb.outerWidth();
+
+    $('#container').mousemove(function(e) {
+        tb.css({
+            left: ((viewport - divWidth) * (e.pageX / viewport)).toFixed(1) + "px"
+        });
+    });
+
+    $('.history-block').on('click', function() {
+        $('.history-block').css('width', '300px');
+        $('.history-block').find('.title').css('width', '260px');
+        $('.history-block .timeline').hide(300);
+
+        $(this).css('width', '600px');
+        $(this).find('.title').css('width', '500px');
+        $(this).find('.timeline').show(800);
+
+        $('#container').off('mousemove').on('mousemove', function(e) {
+            let newLeft = ((viewport - divWidth - 300) * (e.pageX / viewport)).toFixed(1);
+            tb.css({ left: Math.max(300, newLeft) + "px" });
+        });
+    });
+
+    $('.timeline ul li').on('click', function() {
+        $(this).fadeOut(200).fadeIn(200);
+    });
 });
